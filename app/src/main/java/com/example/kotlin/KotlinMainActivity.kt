84cdfae6b599e55dd331995d924f215f6eea13b1
lipengfei <lipengfei@ymt360.com>
2021-06-18 16:54:58 +0800

    携程的简单使用Demo

---
+++ app/src/main/java/com/example/kotlin/KotlinMainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.kotlin
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mrslibutterknife.R
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+class KotlinMainActivity : AppCompatActivity() {
+    var num = 0
+    lateinit var tvNum:TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin_main)
+        tvNum = findViewById<TextView>(R.id.tv_num)
+        var run = findViewById<TextView>(R.id.run)
+
+        val handler: Handler = Handler {
+            tvNum.text = it.what.toString()
+            true
+        }
+        run.setOnClickListener{
+            GlobalScope.launch {
+                dplyNum()
+                var message = Message()
+                message.what = num
+                handler.sendMessage(message)
+            }
+        }
+    }
+
+
+    suspend fun dplyNum(){
+        delay(10000)
+        num++
+    }
+}
